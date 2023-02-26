5f329378824f4ded11f22c8a7efadcf19ec08596
Erdem Selvi <78259899+erdemselvi@users.noreply.github.com>
2023-02-27 00:48:58 +0300

    ilk yükleme

---
+++ app/src/main/java/com/erdemselvi/geriyesayansayac/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.erdemselvi.geriyesayansayac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Soyut Sınıf -Abstract Class
+
+        object : CountDownTimer(10000,1000){
+            override fun onTick(p0: Long) {
+            textView.text="Kalan: ${p0/1000}"
+            }
+
+            override fun onFinish() {
+            textView.text="Kalan:0"
+            }
+
+        }.start()
+
+    }
+}
